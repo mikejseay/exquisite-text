9da29643d28b4e4cc21f497ab9da7ab16179d2d9
Peter Klingelhofer <peterklingelhofer@gmail.com>
2022-11-23 23:00:00 -0600

    fix: copy notification per poem game end screen

--- src/components/PoemLines/index.tsx
+++ src/components/PoemLines/index.tsx
@@ -1,4 +1,8 @@
 import * as React from "react";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import IconButton from "@mui/material/IconButton";
+
 import {
     poemsBody,
 } from "./styles";
@@ -6,7 +10,14 @@ import {
     ILine,
 } from "../../types";
 
-function PoemLines({ poemLines, editorColors }: {poemLines: ILine[], editorColors: Record<string, string>}) {
+import { lineSepString, shortDur } from "../../constants";
+
+function PoemLines({ poemLines, editorColors }: { poemLines: ILine[], editorColors: Record<string, string> }) {
+    const [ isCopied, setIsCopied ] = React.useState<boolean>(false);
+    const copyStatus = isCopied
+        ? <span style={{ color: "gray" }}>Copied.</span>
+        : null;
+
     return (
         <div style={poemsBody}>
             {poemLines.map((line) => {
@@ -19,6 +30,16 @@ function PoemLines({ poemLines, editorColors }: {poemLines: ILine[], editorColor
                     </span>
                 </div>;
             })}
+            <CopyToClipboard text={poemLines.map(line => line.content).join(lineSepString)}
+                onCopy={() => {
+                    setIsCopied(true);
+                    setTimeout(() => setIsCopied(false), shortDur);
+                }}>
+                <IconButton>
+                    <ContentCopyIcon />
+                </IconButton>
+            </CopyToClipboard>
+            {copyStatus}
         </div>
     );
 }
