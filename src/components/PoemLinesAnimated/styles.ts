73736bfd3538df38e5b933a7c34413f8eccdae81
Michael Seay <mikejseay@gmail.com>
2023-10-04 20:05:40 -0400

    remove unneeded properties
    Co-authored-by: peterklingelhofer <peterklingelhofer@gmail.com>

--- src/components/PoemLinesAnimated/styles.ts
+++ src/components/PoemLinesAnimated/styles.ts
@@ -1,11 +1,6 @@
 import * as React from "react";
 
 export const poemBody: React.CSSProperties = {
-    // marginBottom: "2em",
-    // marginTop: "auto",
     marginLeft: "auto",
     marginRight: "auto",
-    // width: "40ch",
-    // outline: "1px dotted black",
-    // textAlign: "center",
 };
