dd31d0b769496e76df0bf90ad0b19098561cde48
Mike Seay <mikejseay@gmail.com>
2023-10-21 13:44:39 -0400

    remove old testing approach

--- src/context/SocketListeners.ts
+++ src/context/SocketListeners.ts
@@ -1,34 +1,24 @@
-import { userInfo as userInfoTestData } from "../data/userInfo";
-import { poemsLines as poemsLinesTestData } from "../data/multiplePoems";
 import { socket } from "./SocketActions";
 import { ILine, ISocketInfoListeners, IUserTableInfo } from "../types";
 
-
 export const socketListeners = ({ setUserInfo, setPoemsLines } : ISocketInfoListeners) => {
 
-    const shouldTest = false;
-    if (shouldTest) {
-        setUserInfo(userInfoTestData);
-        setPoemsLines(poemsLinesTestData);
-    } else {
+    const poemsLinesListener = (myPoemLines: ILine[]) => {
+        setPoemsLines(prevPoemsLines => {
+            return [ ...prevPoemsLines, myPoemLines ];
+        });
+    };
 
-        const poemsLinesListener = (myPoemLines: ILine[]) => {
-            setPoemsLines(prevPoemsLines => {
-                return [ ...prevPoemsLines, myPoemLines ];
-            });
-        };
+    const userTableInfoListener = (info: IUserTableInfo) => {
+        setUserInfo(info);
+    };
 
-        const userTableInfoListener = (info: IUserTableInfo) => {
-            setUserInfo(info);
-        };
+    socket.on("poemLines", poemsLinesListener);
+    socket.on("userTableInfo", userTableInfoListener);
 
-        socket.on("poemLines", poemsLinesListener);
-        socket.on("userTableInfo", userTableInfoListener);
-
-        return () => {
-            socket.off("poemLines", poemsLinesListener);
-            socket.off("userTableInfo", userTableInfoListener);
-        };
-    }
+    return () => {
+        socket.off("poemLines", poemsLinesListener);
+        socket.off("userTableInfo", userTableInfoListener);
+    };
 
 };
