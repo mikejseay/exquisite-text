4839fc1544e9c616a2bd349307aee4377250757e
Mike Seay <mikejseay@gmail.com>
2023-10-21 13:44:24 -0400

    put import in right place

--- src/context/SocketActions.ts
+++ src/context/SocketActions.ts
@@ -1,12 +1,11 @@
 import { io } from "socket.io-client";
+import { socketListeners } from "./SocketListeners";
+import { ISocketInfoListeners } from "../types";
 
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 const serverPath: URL["pathname"] | URL["href"] = isDevelopment
     ? `http://${window.location.hostname}:3000`
     : "/";
-
-import { socketListeners } from "./SocketListeners";
-import { ISocketInfoListeners } from "../types";
 
 export const socket = io(serverPath);
 
