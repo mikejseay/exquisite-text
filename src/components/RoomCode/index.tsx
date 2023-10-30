2b5d2514d266d6c8e0851b890a17f00d555034f9
Mike Seay <mikejseay@gmail.com>
2023-10-29 20:37:49 -0400

    get roomCode from socket context instead

--- src/components/RoomCode/index.tsx
+++ src/components/RoomCode/index.tsx
@@ -1,25 +1,11 @@
 import * as React from "react";
 
-import { socket } from "../../context/SocketActions";
+// import { getRoomCode } from "../../context/SocketRequestors";
+import { useSocketInfo } from "../../context/SocketInfoProvider";
 
 function RoomCode() {
 
-    const [ roomCode, setRoomCode ] = React.useState<string>("");
-
-    // listen for room codes
-    React.useEffect(() => {
-
-        // Event handlers for the line and the deleteLine events are set up for the Socket.IO connection.
-        const roomCodeListener = (info: string) => {
-            setRoomCode(info);
-        };
-        socket.on("roomCode", roomCodeListener);
-        socket.emit("getRoomCode"); // initial populate
-
-        return () => {
-            socket.off("roomCode", roomCodeListener);
-        };
-    }, [ socket ]);
+    const { roomCode } = useSocketInfo();
 
     return (
         <div style={{ textAlign: "center" }} >
