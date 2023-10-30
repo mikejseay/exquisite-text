4e611924456d294a0ad764796ccf94be7ba365c8
Mike Seay <mikejseay@gmail.com>
2023-10-29 20:38:55 -0400

    Lobby listeners

--- src/context/SocketListeners.ts
+++ src/context/SocketListeners.ts
@@ -1,9 +1,19 @@
 import { socket } from "./SocketActions";
-import { ILine, ISocketInfoListeners, IUserTableInfo } from "../types";
+import { IGameSettingsInfo, ILine, ISocketInfoListeners, IUserTableInfo } from "../types";
 
 import { shortDur } from "../constants";
 
-export const socketListeners = ({ setUserInfo, setPoemsLines, setJoinErrorMessage } : ISocketInfoListeners) => {
+export const socketListeners = (
+    {
+        setUserInfo,
+        setPoemsLines,
+        setJoinErrorMessage,
+        setRoomCode,
+        setSettingsEnabled,
+        setLineLength,
+        setNRounds,
+        setNPoems,
+    } : ISocketInfoListeners) => {
 
     const poemsLinesListener = (myPoemLines: ILine[]) => {
         setPoemsLines(prevPoemsLines => {
@@ -20,14 +30,35 @@ export const socketListeners = ({ setUserInfo, setPoemsLines, setJoinErrorMessag
         setTimeout(() => setJoinErrorMessage(""), shortDur);
     };
 
-    socket.on("joinError", joinErrorListener);
+    // Event handlers for the line and the deleteLine events are set up for the Socket.IO connection.
+    const roomCodeListener = (info: string) => {
+        setRoomCode(info);
+    };
+
+    const gameSettingsInfoListener = (info: IGameSettingsInfo) => {
+        setLineLength(info["lineLength"]);
+        setNRounds(info["nRounds"]);
+        setNPoems(info["nPoems"]);
+    };
+
+    const gameSettingsEnabledListener = (enabled: boolean) => {
+        setSettingsEnabled(enabled);
+    };
+
     socket.on("poemLines", poemsLinesListener);
     socket.on("userTableInfo", userTableInfoListener);
+    socket.on("joinError", joinErrorListener);
+    socket.on("roomCode", roomCodeListener);
+    socket.on("gameSettingsInfo", gameSettingsInfoListener);
+    socket.on("gameSettingsEnabled", gameSettingsEnabledListener);
 
     return () => {
-        socket.off("joinError", joinErrorListener);
         socket.off("poemLines", poemsLinesListener);
         socket.off("userTableInfo", userTableInfoListener);
+        socket.off("joinError", joinErrorListener);
+        socket.off("roomCode", roomCodeListener);
+        socket.off("gameSettingsInfo", gameSettingsInfoListener);
+        socket.off("gameSettingsEnabled", gameSettingsEnabledListener);
     };
 
 };
