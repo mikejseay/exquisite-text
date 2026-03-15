2603cc0c7792da609a1c43ceddfb78d197fcd9e6
Peter Klingelhofer <peterklingelhofer@users.noreply.github.com>
2026-03-15 06:45:38 -0500

    Move Canvas TODOs to TODO.md

--- src/screens/Canvas/index.tsx
+++ src/screens/Canvas/index.tsx
@@ -23,25 +23,6 @@ import { useDisableScroll } from "../../hooks/useDisableScroll";
 import { useCanvasResize } from "../../hooks/useCanvasResize";
 import { ScaleDirection, pixelRatio, scalePoints } from "../../utilities/scaleUtils";
 
-/* // TODO:
-    * Now that we're using buttons for canvas controls
-        Make clicking the button show the descriptor tooltip
-        because ipad/iphone has no mouseover
-    * In highly-portrait viewport situations, the line width slider overflows out of view to the left
-    * On iPad, the default line width of using your finger after using pencil is extremely small,
-        but a single dot is very large
-    * End screen aspect ratio not getting set up correctly for small macbook / iPad screen in landscape
-    * "How to play" modal for the drawing version of the game?
-    * Fix bug where user changes system theme it disconnects them and they have to refresh browser
-    * General code quality
-    * One player two drawings, it should alternate between drawings
-    * Two players two drawings, one drawing is complete, player gets no visual feedback that
-        they're waiting on the other player
-    * Spectator view currently defaults to carousel, but if enough real estate is available
-        We could consider laying them out as two columns
-    * Lobby vertically centered, probably too far down the page
-*/
-
 type PointerLikeEvent = {
     clientX: number;
     clientY: number;
